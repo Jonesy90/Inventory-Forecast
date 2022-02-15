51b6d6fad13c6234ca20270900d58a4099dc1281
Michael Jones <michaeljones@Michaels-MacBook-Pro-2.local>
2022-02-15 17:14:38 +0000

    Updates to the daily_average function.

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from models import *
 import argparse
 import pathlib
 import csv
+import datetime
 
 from models import Bookings
 
@@ -15,6 +16,10 @@ args = parser.parse_args()
 booking_uploaded_csv = args.source_file
 
 def menu():
+    """
+    After the CSV has been uploaded, a menu will appear for the user to make a collection.
+    
+    """
     while True:
         print('''
             \n*****MAIN MENU*****:
@@ -33,6 +38,20 @@ def menu():
             \r*****************************''')
 
 
+def daily_average(campaign_external_id):
+    """
+    Calculate the daily average for each booking within the Bookings DB.
+
+    """
+    # print(campaign_external_id)
+    campaign = session.query(Bookings).filter(Bookings.campaign_external_id==campaign_external_id)
+    today = datetime.date.today()
+    
+    for camp in campaign:
+        days_running = (camp.end_date - camp.start_date)
+        print(f'Days Running: {int(days_running.days) + 1}')
+
+
 def add_campaign_bookings():
     """
     Read a CSV file to and add to the Bookings DB.
@@ -44,11 +63,11 @@ def add_campaign_bookings():
         for row in data:
             campaign_external_id = row['\ufeffCampaign External ID']
             campaign_name = row['Campaign Name']
-            start_date = row['Start Date']
-            end_date = row['End Date']
+            start_date = datetime.datetime.strptime(row['Start Date'], '%d/%m/%Y').date()
+            end_date = datetime.datetime.strptime(row['End Date'], '%d/%m/%Y').date()
             booked_impressions = row['Booked Impressions']
             delivered_impressions = row['Delivered Impressions']
-            daily_impressions = row['Daily Impressions']
+            daily_impressions = daily_average(campaign_external_id)
 
             new_booking = Bookings(campaign_external_id=campaign_external_id, campaign_name=campaign_name, start_date=start_date, end_date=end_date, booked_impressions=booked_impressions, delivered_impressions=delivered_impressions, daily_impressions=daily_impressions)
             booking_in_db = session.query(Bookings).filter(Bookings.campaign_external_id==new_booking.campaign_external_id).one_or_none()
@@ -58,8 +77,6 @@ def add_campaign_bookings():
             else:
                 session.add(new_booking)
                 session.commit()
-
-
 
 
 def app():
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy import create_engine, Column, String, Integer, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
 
 engine = create_engine('sqlite:///inventory-forecast.db', echo=False)
 Session = sessionmaker(bind=engine)
@@ -13,8 +14,8 @@ class Bookings(Base):
     id = Column(Integer, primary_key=True)
     campaign_external_id = Column('Campaign External ID', String)
     campaign_name = Column('Campaign Name', String)
-    start_date = Column('Start Date', String)
-    end_date = Column('End Date', String)
+    start_date = Column('Start Date', Date)
+    end_date = Column('End Date', Date)
     booked_impressions = Column('Booked Impressions', Integer)
     delivered_impressions = Column('Delivered Impressions', Integer)
     daily_impressions = Column('Daily Impressions', Integer)
@@ -23,3 +24,14 @@ class Bookings(Base):
         return f'<Bookings(Campaign External ID={self.campaign_external_id}, Campaign Name={self.campaign_name}, Start Date={self.start_date}, End Date={self.end_date}, Booked Impressions={self.booked_impressions}, Delivered Impressions={self.delivered_impressions}, Daily Impressions={self.daily_impressions})>'
 
     
+class Forecast(Base):
+    __tablename__ = 'Forecast'
+
+    id = Column(Integer, primary_key=True)
+    date = Column('Date', String)
+    inventory_available = Column('Inventory Available', Integer)
+    inventory_used = Column('Inventory Used', Integer)
+    inventory_remaining = Column('Inventory Remaining', Integer)
+
+    def __repr__(self):
+        return f'<Forecast(Date={self.date}, Inventory Available={self.inventory_available}, Inventory Used={self.inventory_used}, Inventory Remaining={self.inventory_remaining})>'
