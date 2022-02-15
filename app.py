b6bd4abb98de6d70ec542712acfee14a12caec86
Michael Jones <michaeljones@Michaels-MacBook-Pro-2.local>
2022-02-15 16:40:02 +0000

    Able to add data to the Bookings DB. Created a menu for the user to make selections

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+#Internal Imports
+from models import *
+
 #External Imports
 import argparse
 import pathlib
 import csv
+
+from models import Bookings
 
 parser = argparse.ArgumentParser(description='Uploads the CSV, process it.')
 parser.add_argument('source_file', metavar='source_file', type=pathlib.Path, help='Upload the CSV file.')
@@ -13,55 +18,64 @@ def menu():
     while True:
         print('''
             \n*****MAIN MENU*****:
-            \rb : ADD BOOKINGS
             \rf : FORECAST
+            \rd : EXPORT
+            \re : EXIT
             ''')
         users_choice = input('Please select an option: ').lower()
-        if users_choice in ['b, f']:
+        if users_choice in ['f', 'e']:
             return users_choice
+        else:
+            users_choice = input('''
+            \n**********MENU ERROR***********
+            \rInvalid Option
+            \rPress Enter to try again.
+            \r*****************************''')
 
 
 def add_campaign_bookings():
     """
-    Read a CSV file to and add to the Bookings DB.    
+    Read a CSV file to and add to the Bookings DB.
 
     """
     
     with open(booking_uploaded_csv, newline='') as csvfile:
         data = csv.DictReader(csvfile)
         for row in data:
-            campaign_external_id = row['Campaign External ID']
+            campaign_external_id = row['\ufeffCampaign External ID']
             campaign_name = row['Campaign Name']
             start_date = row['Start Date']
             end_date = row['End Date']
             booked_impressions = row['Booked Impressions']
             delivered_impressions = row['Delivered Impressions']
+            daily_impressions = row['Daily Impressions']
+
+            new_booking = Bookings(campaign_external_id=campaign_external_id, campaign_name=campaign_name, start_date=start_date, end_date=end_date, booked_impressions=booked_impressions, delivered_impressions=delivered_impressions, daily_impressions=daily_impressions)
+            booking_in_db = session.query(Bookings).filter(Bookings.campaign_external_id==new_booking.campaign_external_id).one_or_none()
+
+            if booking_in_db != None:
+                pass
+            else:
+                session.add(new_booking)
+                session.commit()
+
 
 
 
 def app():
+    add_campaign_bookings()
     app_running = True
     while app_running:
         choice = menu()
-        if choice == 'b':
-            add_campaign_bookings()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if choice == 'f':
+            print('FORECAST')
+        elif choice == 'd':
+            print('EXPORT')
+        elif choice == 'e':
+            print('EXIT')
+            app_running = False
 
 
 if __name__ == '__main__':
+    Base.metadata.create_all(engine)
     app()
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, String, Integer, Float
+from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -20,6 +20,6 @@ class Bookings(Base):
     daily_impressions = Column('Daily Impressions', Integer)
 
     def __repr__(self):
-        return f'<Booking(Campaign External ID={self.campaign_external_id}, Campaign Name={self.campaign_name}, Start Date={self.start_date}, End Date={self.end_date}, Booked Impressions={self.booked_impressions}, Delivered Impressions={self.delivered_impressions}, Daily Impressions={self.daily_impressions})>'
+        return f'<Bookings(Campaign External ID={self.campaign_external_id}, Campaign Name={self.campaign_name}, Start Date={self.start_date}, End Date={self.end_date}, Booked Impressions={self.booked_impressions}, Delivered Impressions={self.delivered_impressions}, Daily Impressions={self.daily_impressions})>'
 
     
